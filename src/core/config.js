d0570a46eda92060d72d087311ff908da6dbc491
melih <melihispir@hotmail.com>
2020-12-01 17:35:27 +0300

    firebase config add

---
+++ src/core/config.js
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDtZbeEjaSxQQQzz-TxnnWEQ0Z8yyrotb8",
+  authDomain: "covidapp-6a400.firebaseapp.com",
+  databaseURL: "https://covidapp-6a400.firebaseio.com",
+  projectId: "covidapp-6a400",
+  storageBucket: "covidapp-6a400.appspot.com",
+  messagingSenderId: "1052686237171",
+  appId: "1:1052686237171:web:076b57826352e846966686",
+  measurementId: "G-Q3N2GNN3N0"
+};
